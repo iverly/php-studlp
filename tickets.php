48da5dbed39b89658e65aeef4fa00f186f525157
unknown <luqui31@gmail.com>
2019-12-06 03:20:59 +0100

    Tickets

---
+++ tickets.php
@@ -0,0 +1,13 @@
+<?php
+    /* DataBase connection */
+    require_once('bdd.php');
+
+    $LastName = htmlspecialchars($_POST['contactLastName']);
+    $firstName = htmlspecialchars($_POST['contactFirstName']);
+    $mail = htmlspecialchars($_POST['contactEmail']);
+    $message = htmlspecialchars($_POST['contactMessage']);
+    $insertTicket = $bdd -> prepare("INSERT INTO tickets(LastName, FirstName, Mail, MessageU) VALUES (?, ?, ?, ?)");
+    $insertTicket -> execute(array($lastName, $firstName, $mail, $message));
+    $ticket = array("Ticket"=>"Created");
+    json_encode($ticket);
+?>
